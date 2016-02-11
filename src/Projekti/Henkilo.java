9c9850771417b6241abc026a1371edc8d34d5396
AsseTheEngineer <samuli.kakonen@metropolia.fi>
2016-02-11 12:32:42 +0200

    Henkiö classin pohja käyttäkää tässä luokassa olevia metodeja. Esim baarissa on juomia 3 1=kalja 2=sidukka jne.. ostaJuoma() { henkilo.Maksu(1); }

--- src/Projekti/Henkilo.java
+++ src/Projekti/Henkilo.java
@@ -1,14 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Projekti;
 
-/**
- *
- * @author Samuli Käkönen
- */
 public class Henkilo {
+    private final int ika;
+    private double lompakko;
+    private final String etunimi;
+    private final String sukunimi;
+    private double humalatila;
+    private int juomat;
+    
+    public Henkilo(int age, double money, String etuNimi, String sukuNimi, double humalaTila, int juomat) {
+        this.ika = age;
+        this.lompakko = money;
+        this.etunimi = etuNimi;
+        this.sukunimi = sukuNimi;
+        this.humalatila = humalaTila;
+        this.juomat = juomat;
+    }
+    
+    int getIka() {
+        return ika;
+    }
+    
+    double getRaha() {
+        return lompakko;
+    }
+
+    public String getEtunimi() {
+        return etunimi;
+    }
+
+    public String getSukunimi() {
+        return sukunimi;
+    }
+
+    public double getHumalatila() {
+        return humalatila;
+    }
+
+    public int getJuomat() {
+        return juomat;
+    }
+
+    public void setLompakko(double lompakko) {
+        this.lompakko = lompakko;
+    }
+
+    public void setHumalatila(double humalatila) {
+        this.humalatila = humalatila;
+    }
+
+    public void setJuomat(int juomat) {
+        this.juomat = juomat;
+    }
+    
+    public boolean uusiMaksu(double maksu) {
+        boolean success = false;
+        if (lompakko >= maksu) {
+            lompakko -= maksu;
+            success = true;
+        }else if (lompakko > maksu) {
+            success = false;
+        }
+        return success;
+    }
+
+    @Override
+    public String toString() {
+        return "Henkilo{" + "ika=" + ika + ", lompakko=" + lompakko + ", etunimi=" + etunimi + ", sukunimi=" + sukunimi + ", humalatila=" + humalatila + ", juomat=" + juomat + '}';
+    }
+    
     
 }
