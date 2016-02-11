b5214f0f1179e15ea19784cb9d71e64b9fefd5c1
markuspokkinen <markuspokkinen@gmail.com>
2016-02-11 12:03:47 +0200

    Koti

---
+++ src/Projekti/Koti.java
@@ -0,0 +1,44 @@
+package Projekti;
+
+public class Koti {
+   private double saastot;
+   private int juomat;
+    
+    public Koti(int saastot, int juomat){
+        this.saastot = saastot;
+        this.juomat = juomat;
+    }
+
+    public void setSaastot(double saastot) {
+        this.saastot = saastot;
+    }
+
+    public void setJuomat(int juomat) {
+        this.juomat = juomat;
+    }
+
+    public double getSaastot() {
+        return saastot;
+    }
+
+    public int getJuomat() {
+        return juomat;
+    }
+    void juodaan(){
+        System.out.println("Ei jaksa, juodaan kotona");
+    }
+    void juotu(){
+        System.out.println("no niin juotu: "+juomat+" Rahan edestä");
+    }
+    void otaRahaa(int summa){
+        //säästoiltä otetaan rahaa
+        if(saastot > summa){
+        saastot = saastot - summa;
+        System.out.println("saastoja jäljellä: "+ saastot);}
+        
+        else System.out.println("Ei ole rahaa!!");
+    }
+    
+
+    
+}
