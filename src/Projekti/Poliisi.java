d25a9207ac52e3df9deee4e0a37b49c80c4f2f6d
markuspokkinen <markuspokkinen@gmail.com>
2016-02-16 12:08:19 +0200

    poliisi maksa sakot ja main.

--- src/Projekti/Poliisi.java
+++ src/Projekti/Poliisi.java
@@ -1,19 +1,24 @@
 package Projekti;
 
 public class Poliisi {
-   private Henkilo henk;
-    
-    private double rahaTililla =  henk.getRaha();
+  Henkilo henk;
+    final int sakot;
     int huomautukset = 2;
  
-    
+    public Poliisi(int sakot){
+     this.sakot = sakot;   
+    }
  
-   void maksaSakot(double sakot){
-       if(sakot <= rahaTililla){
-       rahaTililla = rahaTililla - sakot;
-       System.out.println("Rahaa jäljellä: "+ rahaTililla);}
-       else joudutPutkaan();
+   String maksaSakot(Henkilo henkilo){
+       boolean onnistuiko;
+       onnistuiko = henkilo.uusiMaksu(sakot);
+       String kommentti;
        
+       if(onnistuiko == true)
+           kommentti = "sakkojen maksu onnistui";
+       else
+           kommentti = "sakkojen maksu epäonnistui";
+       return kommentti;
    }
    void joudutPutkaan(){
             System.out.println("Mitä tuli tehtyä jouduit putkaan");
--- src/Projekti/TestMain.java
+++ src/Projekti/TestMain.java
@@ -6,11 +6,12 @@ public class TestMain {
         System.out.println("Testi");
         
         Koti koti;
-        Poliisi pol = new Poliisi();
-        Henkilo henk;
+        Poliisi pol = new Poliisi(50);
+        Henkilo henk = new Henkilo(12,100,"kalle","kaapo",1.3,13); //int age, double money, String etuNimi, String sukuNimi, double humalaTila, int juomat
         Puisto puisto;
  
-       
+        System.out.println(pol.maksaSakot(henk));
+        System.out.println(henk.getRaha());
         
     }
     
