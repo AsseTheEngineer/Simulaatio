20d20ce074d8034a7b45e6079b1477be4b16f58a
markuspokkinen <markuspokkinen@gmail.com>
2016-03-01 12:58:25 +0200

    Merge origin/test into test1
    Conflicts:
    	src/Projekti/Baari.java
    	src/Projekti/Henkilo.java
    	src/Projekti/Paikka.java
    	src/Projekti/Poliisi.java
    	src/Projekti/Puisto.java
    	src/Projekti/TestMain.java

--- src/Projekti/Paikka.java
+++ src/Projekti/Paikka.java
@@ -68,25 +68,25 @@ public class Paikka {
         
         this.random = Math.random();
         //System.out.println(tapahtumat);
-        if (random >= 0 && random < 0.2) { // katu
+        if (random <= 0.2) { // katu
             tapahtumat += "[" + kello() + ", katu]";
             katu();
 
-        } else if (random >= 0.2 && random < 0.4) { //baari
+        } else if (random > 0.2 && random <= 0.4) { //baari
             random = (int) (Math.random() * baarit.size());
             tapahtumat += "[" + kello() + ", " + baarit.get((int)random).getNimi() + "]";
             baari(baarit.get((int)random));
 
-        } else if (random >= 0.4 && random < 0.6) { //koti
+        } else if (random > 0.4 && random <= 0.6) { //koti
             tapahtumat += "[" + kello() + ", Koti]";
             koti();
             //tohon viel loput paikat   
 
-        } else if (random >= 0.6 && random < 0.8) { // kaverin kämppä
+        } else if (random > 0.6 && random <= 0.8) { // kaverin kämppä
             tapahtumat += "[" + kello() + ", Kaverin kämppä]";
             kaverinKoti();
 
-        } else if (random >= 0.8 && random <= 1) { //puisto
+        } else if (random > 0.8 && random <= 1) { //puisto
             tapahtumat += "[" + kello() + ", puisto]";
             puisto();
         }
@@ -95,12 +95,12 @@ public class Paikka {
 
     public void baari(Baari baari) {
         random = Math.random();
-        if (random < 0.33) {
+        if (random <= 0.33) {
             random = (int) (Math.random() * 5);
             tapahtumat += baari.ostaJuoma((int) random, henkilo) + "\n";
-        } else if (random >= 0.33 && random < 0.66) {
+        } else if (random > 0.33 && random <= 0.66) {
             tapahtumat += baari.tanssi() + "\n";
-        } else if (random >= 0.66) {
+        } else if (random > 0.66) {
             tapahtumat += baari.puhuPaskaa() + "\n";
         }
     }
@@ -108,23 +108,25 @@ public class Paikka {
     public void katu() {
         //Muokkaa uusiks
         random = Math.random();
-        tapahtumat += henkilo.juoOmaJuoma() + "\n";
-        {
-            if (random >= 0 && random <= 0.33) {
+
+        
+            if (random <= 0.33) {
                 tapahtumat += poliisi.huomautus(henkilo) + "\n"; //"[" + date + "] " +
+            } else {
+                 tapahtumat += henkilo.juoOmaJuoma() + "\n";
             }
-        }
+        
     }
 
     public void kaverinKoti() {
         double random = Math.random();
-        if (random >= 0 && random < 0.25) {
+        if (random <= 0.25) {
             tapahtumat += kaveri.juoJuoma(henkilo) + "\n";
-        }else if (random >= 0.25 && random < 0.5) {
+        }else if (random > 0.25 && random <= 0.5) {
             tapahtumat += kaveri.otaKaverinJuoma(henkilo) + "\n";
-        }else if (random >= 0.5 && random < 0.75) {
+        }else if (random > 0.5 && random <= 0.75) {
             tapahtumat += kaveri.otaRahaa(henkilo) + "\n";
-        }else if (random >= 0.75 && random <= 1) {
+        }else if (random > 0.75 && random <= 1) {
             tapahtumat += kaveri.puhuPaskaa(henkilo) + "\n";
         }
     }
@@ -132,11 +134,11 @@ public class Paikka {
     public void koti() {
         random = Math.random();
         int sum = (int)(Math.random() * koti.getSaastot());
-        if (random < 0.4) {
+        if (random <= 0.4) {
             tapahtumat += koti.otaJuomia() + "\n";
-        }else if (random >= 0.4 && random < 0.7) {
+        }else if (random > 0.4 && random <= 0.7) {
             tapahtumat += koti.omaJuoma() + "\n";
-        }else if (random >= 0.7) {
+        }else if (random > 0.7) {
             tapahtumat += koti.otaRahaa(sum) + "\n";
         }
 
@@ -144,7 +146,7 @@ public class Paikka {
 
     public void puisto() {
         random = Math.random();
-        if (random > 0 && random < 0.5) {
+        if (random <= 0.5) {
             tapahtumat += puisto.juoOmajuoma(henkilo) + "\n";
         }else {
             tapahtumat += puisto.pummiJuoma(henkilo) + "\n";
