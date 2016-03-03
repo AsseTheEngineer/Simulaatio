ab7d1bca1c41e37884b9b929c4e7ad4cfe4b2f58
markuspokkinen <markuspokkinen@gmail.com>
2016-03-03 12:54:07 +0200

    Merge origin/test into test1
    Conflicts:
    	src/Projekti/Baari.java
    	src/Projekti/Henkilo.java
    	src/Projekti/Paikka.java
    	src/Projekti/Poliisi.java
    	src/Projekti/Puisto.java
    	src/Projekti/TestMain.java

--- src/Projekti/SimulaatioMain.java
+++ src/Projekti/SimulaatioMain.java
@@ -19,18 +19,18 @@ import javafx.stage.Stage;
  * @author Asse
  */
 public class SimulaatioMain extends Application {
-    
+
     public static Henkilo jarno = new Henkilo(100, "Jarno", "Penttilä", 0, 5);
-        
-    public static Baari prkl = new Baari(4,4,5,5, "prkl");
+
+    public static Baari prkl = new Baari(4, 4, 5, 5, "prkl");
     public static Baari boothill = new Baari(4, 5, 6, 6, "Boothill");
-        
+
     public static Poliisi poliisi = new Poliisi(40);
     public static Koti jarnonKoti = new Koti(jarno);
     public static Kaverinkamppa makenKoti = new Kaverinkamppa(5, 50);
     public static Puisto kaivopuisto = new Puisto(poliisi);
     public static Paikka paikka = new Paikka(jarno, poliisi, jarnonKoti, makenKoti, kaivopuisto);
-    
+
     public static void simuloi() {
         int gameOver = 0;
         while (gameOver != 1) {
@@ -38,20 +38,20 @@ public class SimulaatioMain extends Application {
             //System.out.println("\n" + jarno.getHumalatila() + "\n");
             if (jarno.getHumalatila() >= 10) {
                 gameOver = 1;
-            }else if (paikka.kello().equals("04:20")) { //#sweg
+            } else if (paikka.kello().equals("04:20")) { //#sweg
                 gameOver = 1;
-            }else if (jarno.getHuomautukset() >= 3) {
+            } else if (jarno.getHuomautukset() >= 3) {
                 gameOver = 1;
-            }  
+            }
         }
     }
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Ikkuna.fxml"));
-        
+
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
         stage.show();
     }
@@ -65,5 +65,5 @@ public class SimulaatioMain extends Application {
         paikka.addBaari(boothill);
         launch(args);
     }
-    
+
 }
