650475d68654b86f2cffadb1af05e4e8f00f4194
markuspokkinen <markuspokkinen@gmail.com>
2016-03-03 13:47:59 +0200

    Merge origin/testioksa into test1
    Conflicts:
    	src/Projekti/Koti.java
    	src/Projekti/SimulaatioMain.java

--- src/Projekti/Poliisi.java
+++ src/Projekti/Poliisi.java
@@ -1,61 +1,60 @@
 package Projekti;
 
 public class Poliisi {
-  Henkilo henkilo;
+
+    Henkilo henkilo;
     public int sakot;
+
     /*Otin huomautukset pois ja siirsin ne "henkilölle" 
     koska henkilö on olio ja jos huomautukset olisi ollut poliisi oliolla ei usesalle henkilölle pystyisi antaa huomautuksia
     koska huomautukset on poliisilla
-    */
+     */
     public Poliisi() {
         this.sakot = 50;
     }
-    
-    
-    public Poliisi(int sakot){
-     this.sakot = sakot;
-     //this.henkilo = henkilo; <-- mitäköhä toiki tekee
+
+    public Poliisi(int sakot) {
+        this.sakot = sakot;
+        //this.henkilo = henkilo; <-- mitäköhä toiki tekee
     }
- 
+
     public void setSakot(int sakko) {
         this.sakot = sakko;
     }
-    
-    public String maksaSakot(Henkilo henkilo){
-       boolean onnistuiko;
-       onnistuiko = henkilo.uusiMaksu(sakot);
-       String kommentti;
-       
-       if(onnistuiko == true)
-           kommentti = "sakkojen maksu onnistui";
-       else
-           kommentti = "sakkojen maksu epäonnistui";
-       return kommentti;
-   }
-   
-   
+
+    public String maksaSakot(Henkilo henkilo) {
+        boolean onnistuiko;
+        onnistuiko = henkilo.uusiMaksu(sakot);
+        String kommentti;
+
+        if (onnistuiko == true) {
+            kommentti = "sakkojen maksu onnistui";
+        } else {
+            kommentti = "sakkojen maksu epäonnistui";
+        }
+        return kommentti;
+    }
+
     /* Toi metodi on periaattees turha koska ton kommentoi voi laittaa huomautus metodiin
     String joudutPutkaan(){
             return "Mitä tuli tehtyä jouduit putkaan";
    }*/
-   
-   
     //Tohon lisäsin henkilön mikä annetaa kun kutsutaan tota metodia
     //(Auttaa siinä että voidaan antaa se huomautus sille henkilölle ei poliisille)
-    String huomautus(Henkilo henkilo){
-       int huomautukset = henkilo.getHuomautukset();
-       String kommentti = "";
-       
-       if(huomautukset <= 1){
+    String huomautus(Henkilo henkilo) {
+        int huomautukset = henkilo.getHuomautukset();
+        String kommentti = "";
+
+        if (huomautukset <= 1) {
             henkilo.lisaaHuomautus();
             kommentti = "Korkkaat juoman repustasi huomaamatta, että herra konstaapeli on selkäsi takana. Pääset kuitenkin pälkähästä pelkällä huomautuksella.";
-       }else if(huomautukset == 2){
-           kommentti ="sakot kommentti 2";
-           maksaSakot(henkilo);
-       } else if(huomautukset > 2){
+        } else if (huomautukset == 2) {
+            kommentti = "sakot kommentti 2";
+            maksaSakot(henkilo);
+        } else if (huomautukset > 2) {
             kommentti = "Samainen konstaapeli, joka on sinulle jo aikaisemmin antanut huomautuksen sattuu jälleen kerran paikalle. Tällä kertaa hän ei ole yhtä suvaitsevainen. Pääset maijan kyydillä putkaan.";
-       }
-       
-       return kommentti;
-       }
+        }
+
+        return kommentti;
+    }
 }
